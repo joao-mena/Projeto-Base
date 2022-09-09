881db3f1c01936ceb3ea571f05930647630b4655
joao.mena <jsemeaomen@hotmail.com>
2022-09-08 23:20:46 -0300

    ajustes

--- src/js/app.js
+++ src/js/app.js
@@ -1,58 +1,57 @@
 import * as flsFunctions from "./modules/functions.js";
-import Swiper from 'swiper';
+import Swiper from "swiper";
 import {
-	Navigation,
-	EffectFade,
-	Pagination,
-	Thumbs,
-	Controller,
-	FreeMode,
-	Scrollbar,
-	A11y,
-	Mousewheel,
-} from 'swiper';
-import AOS from 'aos';
+  Navigation,
+  EffectFade,
+  Pagination,
+  Thumbs,
+  Controller,
+  FreeMode,
+  Scrollbar,
+  A11y,
+  Mousewheel,
+} from "swiper";
 
 flsFunctions.isWebp();
 
-const swiper = new Swiper('.auctions-slider', {
-	navigation: {
-		nextEl: '.swiper-button-next1',
-		prevEl: '.swiper-button-prev1' // Включить стрелочки
-	},
-	modules: [Navigation, FreeMode], // Модули которые будут использоваться
-	autoHeight: true, // Автовысота
-	speed: 500, // Скорость прокрутки слайдера
-	slidesPerView: 3, // Количество слайдов которые будут видны,
-	spaceBetween: 42,
-	freeMode: true,
-	grabCursor: true
+const swiper = new Swiper(".auctions-slider", {
+  navigation: {
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1", // Enable arrows
+  },
+  modules: [Navigation, FreeMode], // Modules to be used
+  autoHeight: true, // auto height
+  speed: 500, // Slider scroll speed
+  slidesPerView: 3, // The number of slides that will be visible
+  spaceBetween: 42,
+  freeMode: true,
+  grabCursor: true,
 });
 
-const swiper2 = new Swiper('.popular-slider', {
-	navigation: {
-		nextEl: '.swiper-button-next2',
-		prevEl: '.swiper-button-prev2' // Включить стрелочки
-	},
-	modules: [Navigation, FreeMode], // Модули которые будут использоваться
-	autoHeight: true, // Автовысота
-	speed: 500, // Скорость прокрутки слайдера
-	slidesPerView: 2.5, // Количество слайдов которые будут видны,
-	spaceBetween: 42,
-	freeMode: true,
-	grabCursor: true
+const swiper2 = new Swiper(".popular-slider", {
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2", // Enable arrows
+  },
+  modules: [Navigation, FreeMode], // Modules to be used
+  autoHeight: true, // auto height
+  speed: 500, // Slider scroll speed
+  slidesPerView: 2.5, // The number of slides that will be visible
+  spaceBetween: 42,
+  freeMode: true,
+  grabCursor: true,
 });
 
-const swiper3 = new Swiper('.categories-slider', {
-	navigation: {
-		nextEl: '.swiper-button-next3',
-		prevEl: '.swiper-button-prev3' // Включить стрелочки
-	},
-	modules: [Navigation, FreeMode], // Модули которые будут использоваться
-	autoHeight: true, // Автовысота
-	speed: 500, // Скорость прокрутки слайдера
-	slidesPerView: 3, // Количество слайдов которые будут видны,
-	spaceBetween: 42,
-	freeMode: true,
-	grabCursor: true
+const swiper3 = new Swiper(".categories-slider", {
+  navigation: {
+    nextEl: ".swiper-button-next3",
+    prevEl: ".swiper-button-prev3", // Enable arrows
+  },
+  modules: [Navigation, FreeMode], // Modules to be used
+  autoHeight: true, // auto height
+  speed: 500, // Slider scroll speed
+  slidesPerView: 3, // The number of slides that will be visible
+  spaceBetween: 42,
+  freeMode: true,
+  grabCursor: true,
 });
