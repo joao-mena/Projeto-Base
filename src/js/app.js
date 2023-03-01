f36e982bafc60f3cc82a423de491d816e3d281b7
joao.mena <jsemeaomen@hotmail.com>
2023-02-28 21:22:51 -0300

    restart

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import * as header from "./modules/header.js";
-import * as flsFunctions from "./modules/functions.js";
-import * as swiper from "./modules/swiper.js";
-
-flsFunctions.isWebp();
-header.header();
-swiper.swiperJS();
